10a572f397efc26a30cb5482c3e3d8966bf248cc
CURVX <chakrawarti.anupam@gmail.com>
2021-05-03 18:37:10 +0530

    Applied middleware Logger to log state changes in console

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,9 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 // import cakeReducer from "./cake/cakeReducer";
 import rootReducer from "./rootReducer";
+import logger from "redux-logger";
 
 // Can accept only one reducer but using combineReducer
 // we can pass multiple reducer
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
